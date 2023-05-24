db002c0cc4c511c9ea308d14fce7fe787a025eee
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-05-24 15:26:37 -0300

    Prueba del GPIO 1 (tecla 'w') linea 56 al 77

--- ejercicio01/app.s
+++ ejercicio01/app.s
@@ -53,5 +53,28 @@ loop0:
 	//---------------------------------------------------------------
 	// Infinite Loop
 
+	//AQUÍ EMPIEZA MI PRUEBA PARA TOCAR LA TECLA 'W' Y CAMBIAR DE ROSA A VERDE LA PANTALLA
+	movz x15, 0x00, lsl 16
+	movk x15, 0xFF00, lsl 00
+	mov x0, x20 //guardo en x16 la dirección base del framebuffer
+leyendo:
+	ldr w10, [x9, GPIO_GPLEV0]//leo el estado de los GPIO 0 - 31
+	//Al hacer AND revela el estado del bit 2,ie, el estado del GPIO 1
+	and w11, w10, 0b00000010
+	SUBS wzr, w11, 0b00000010
+	B.NE leyendo
+	mov x2, SCREEN_HEIGH         // Y Size
+loop3:
+	mov x1, SCREEN_WIDTH         // X Size
+loop2:
+	stur w15,[x0]  // Colorear el pixel N
+	add x0,x0,4    // Siguiente pixel
+	sub x1,x1,1    // Decrementar contador X
+	cbnz x1,loop2  // Si no terminó la fila, salto
+	sub x2,x2,1    // Decrementar contador Y
+	cbnz x2,loop3  // Si no es la última fila, salto
+	//AQUÍ FINALIZA MI PRUEBA, SI SE TOCA 'W' DE NUEVO, 
+	//NO SE REGRESA AL COLOR ANTERIOR YA QUE FINALIZO EL PROGRAMA
+
 InfLoop:
 	b InfLoop
