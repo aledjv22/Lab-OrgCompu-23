c237ccb8688ecfde264dcd525b0580ffc292635b
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-05-31 13:43:17 -0300

    Lista pero sin comentar la funcion circulo

--- ejercicio01/app.s
+++ ejercicio01/app.s
@@ -1,8 +1,6 @@
 	.equ SCREEN_WIDTH,   640
 	.equ SCREEN_HEIGH,   480
 	.equ BITS_PER_PIXEL, 32
-    .equ CORD_Y,         240
-    .equ CORD_X,         320
 
 	.equ GPIO_BASE,    0x3f200000
 	.equ GPIO_GPFSEL0, 0x00
@@ -92,11 +90,11 @@ separador:
 
 arbustos:
     //cordenadas, radio y color ale jejeje
-    mov x2,320
-    mov x1,240
+    mov x17,320 //x
+    mov x15,240 //y
     mov x4,40
-    movz x3, 0xFF, lsl 16 //color rojo
-    bl draw_circle
+    movz x10, 0xFF, lsl 16 //color rojo
+    bl circulo
 
     b InfLoop
 
@@ -125,63 +123,35 @@ rectangulo://x15=PY;x16=FY;x17=PX;x18=FX;w10=color;x19=auxX;x21=aux
         br lr //Retorno a la ubicación de la llamada almacenada en LR (una alternativa es 'ret')
 
 //Funcion encargada de dibujar un circulo
-draw_circle:
-	// Draws a circle given a (x0, y0) center coords (x2, x1) a radius (x4) and a color (x3)
-
-	mov x16, x30 
-
-	mov x6, x2           // Save center coords
-	mov x5, x1
-
-	add x7, x1, x4       // Save end of vertical lines
-	add x8, x2, x4       // Save end of horizontal line
-
-	sub x2, x2, x4
-	sub x1, x1, x4         // set the coords to the leftmost top corner of the square r^2
-
-	smull x15, w4, w4       // save r^2
-
-loopcircle:
-
-	sub x14, x2, x6 
-	smull x14, w14, w14       // (X - x0)^2
-
-	sub x13, x1, x5
-	smull x13, w13, w13       // (Y - y0)^2
-
-	add x13, x13, x14       // add results
-
-	cmp x15, x13
-	b.le skip_paint
-
-	bl paint_pixel
-
-skip_paint:
-
-	add x2, x2, 1
-	cmp x8, x2
-	b.ne loopcircle
-
-	sub x2, x2, x4
-	sub x2, x2, x4          // Reset x coord
-
-	add x1, x1, 1
-	cmp x7, x1
-	b.ne loopcircle
-	
-	ret x16 
-
-paint_pixel:      
-	// Paint a pixel given coords (x,y) (x2,x1) and a color (x3)
-	mov x11, SCREEN_WIDTH
-	mov x12, 4         // Save the width and the number 4 in x11 and x12
-	mul x11, x1, x11   // Calculate the pixel position
-	add x11, x2, x11
-	mul x11, x12, x11
-	add x0, x0, x11    // Set x0 to the position
-	stur w3, [x0]      // Paint the pixel
-	mov x0, x20        // Reset x0
-	br lr
+circulo:
+	// Draws a circle given a (x0, y0) center coords (x17, x15) a radius (x4) and a color (x10)
+	mov x16,lr //guardo el valor del lr(x30)
+	mov x18,x17 // Save center coords
+	mov x19,x15
+	add x21,x15,x4 // Save end of vertical lines
+	add x25,x17,x4 // Save end of horizontal line
+	sub x17,x17,x4
+	sub x15,x15,x4 // set the coords to the leftmost top corner of the square r^2
+	smull x22,w4,w4 // save r^2
+    loopcircle:
+        sub x23,x17,x18 
+        smull x23,w23,w23 // (X - x0)^2
+        sub x24,x15,x19
+        smull x24,w24,w24 // (Y - y0)^2
+        add x24,x24,x23 // add results
+        cmp x22, x24
+        b.le skip_paint
+        bl pintar_pixel
+    skip_paint:
+        add x17,x17,1
+        cmp x25,x17
+        b.ne loopcircle
+        sub x17,x17,x4
+        sub x17,x17,x4 // Reset x coord
+        add x15,x15,1
+        cmp x21,x15
+        b.ne loopcircle
+        ret x16 
 
 pintar_pixel:
     mov x21,SCREEN_WIDTH //x21 = 640
@@ -190,7 +160,6 @@ pintar_pixel:
     lsl x21,x21,2 //4 * (X + Y*640)
     add x21,x0,x21 //direIni + 4*(X + Y*640)
     stur w10,[x21] //pinto el pixel
-    mov x0,x20
     br lr
 
 InfLoop:
